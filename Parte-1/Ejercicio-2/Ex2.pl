164ae4eba54dfb26ab9217b429abfee97ca90fbc
nicomoncarz <nicolas@nicolas-hp-envy.fibertel.com.ar>
2021-05-02 19:56:10 -0300

    Ejercicio 2: Arreglando el blastp cambiando de version

--- Parte-1/Ejercicio-2/Ex2.pl
+++ Parte-1/Ejercicio-2/Ex2.pl
@@ -2,17 +2,12 @@
 
 use Bio::SeqIO;
 use Bio::Tools::Run::StandAloneBlastPlus;
-use Bio::Tools::Run::RemoteBlast;
-my $prog = 'blastp';
-my $db   = 'swissprot';
-my $e_val= '1e-10';
- 
-@remote_params = ( '-prog' => $prog,
-       '-data' => $db,
-       '-expect' => $e_val,
-       '-readmethod' => 'SearchIO' );
- 
-$remote_factory = Bio::Tools::Run::RemoteBlast->new(@remote_params);
+
+# blast against remote database
+$remote_factory = Bio::Tools::Run::StandAloneBlastPlus->new(
+    -db_name => 'nr',
+    -remote => 1
+);
 
 # blast against local database
 $local_factory = Bio::Tools::Run::StandAloneBlastPlus->new(
@@ -31,41 +26,7 @@ $local_factory = Bio::Tools::Run::StandAloneBlastPlus->new(
 
 foreach (@fasta_files) {
     $fastaInputFileName = $_;
-    # Local DB
     $outputFileName = substr($fastaInputFileName, 0, index($fastaInputFileName, '.'));
     $local_factory->blastp( -query => "../Ejercicio-1/$fastaInputFileName", -outfile => "$outputFileName-blast.out");
-    # Remote DB
-    $v = 1; 
-    my $str = Bio::SeqIO->new(-file=>"../Ejercicio-1/$fastaInputFileName" , -format => 'fasta' );
-    while (my $input = $str->next_seq()){
-        #Blast a sequence against a database:
-        my $r = $remote_factory->submit_blast($input);
-        print STDERR "waiting..." if( $v > 0 );
-        while ( my @rids = $remote_factory->each_rid ) {
-            foreach my $rid ( @rids ) {
-                my $rc = $remote_factory->retrieve_blast($rid);
-                if( !ref($rc) ) {
-                    if( $rc < 0 ) {
-                        $remote_factory->remove_rid($rid);
-                    }
-                    print STDERR "." if ( $v > 0 );
-                } else {
-                    my $result = $rc->next_result();
-                    #save the output
-                    my $outputRemoteFilename = "$outputFileName-remote-blast.out";
-                    $remote_factory->save_output($outputRemoteFilename);
-                    $remote_factory->remove_rid($rid);
-                    print "\nQuery Name: ", $result->query_name(), "\n";
-                    while ( my $hit = $result->next_hit ) {
-                        next unless ( $v > 0);
-                        print "\thit name is ", $hit->name, "\n";
-                        while( my $hsp = $hit->next_hsp ) {
-                            print "\t\tscore is ", $hsp->score, "\n";
-                        }
-                    }
-                }
-            }
-        }
-    }
-
+    $remote_factory->blastp( -query => "../Ejercicio-1/$fastaInputFileName", -outfile => "$outputFileName-remote-blast.out");
 }
--- Parte-1/Ejercicio-2/README.md
+++ Parte-1/Ejercicio-2/README.md
@@ -8,17 +8,16 @@ realiza un Blast por cada secuencia input) y escriba el resultado (blast output)
 ## Instalación local de BLAST
 
 - Cumplir con la instalacion necesaria del Ejercicio-1
-- Bajar BLAST localmente del FTP del NCBI: https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ o usando `sudo apt-get install ncbi-blast+`
+- Bajar BLAST(2.8.1-1. La version 2.9.0 tiene issues en el comando blastp) localmente del FTP del NCBI: `http://ftp.br.debian.org/debian/pool/main/n/ncbi-blast+/ncbi-blast+_2.8.1-1+deb10u1_amd64.deb` y luego instalarlo con `sudo dpkg -i ncbi-blast+_2.8.1-1+deb10u1_amd64.deb`. Como requiere de python es necesario luego realizar `sudo apr-get install python` y luego corregir las dependencias de los paquetes `sudo apt --fix-broken install`
 - Bajarse la base de datos
-- Instalar Blast remote con `sudo apt install libbio-tools-run-remoteblast-perl`
 
 ```
 wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/swissprot.gz 
 ```
 
-Descomprimirla en un directorio por ejemplo: `ncbi-blast- 2.3.0+/data/`
+Descomprimirla en un directorio por ejemplo: i`ncbi-blast- 2.3.0+/data/`
 
-- Usar el comando `ncbi-blast-2.3.0+/bin/makeblastdb` sobre el archivo swissprot (el original ya está en formato FASTA) para darle formato de BLAST DB. Dependiendo de la versión de Blast suite que tengan instalado puede que en vez de makeblastdb deban utilizar el comando formatdb.
+- Usar el comando `ncbi-blast-2.3.0+/bin/makeblastdb` sobre el archivo swissprot (el original ya está en formato FASTA) para darle formato de BLAST DB. Dependiendo de la versión de Blast suite que tengan instalado puede que en vez de makeblastdb deban utilizar el comando formatdb o `makeblastdb -in swissprot -dbtype prot -parse_seqids -out swissprot`
 
 ## Dependencias
 
